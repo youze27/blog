adaa84406a5a4b79636955ed7c2f947e8df1f9f8
siyuan <youze27@163.com>
2025-12-01 22:59:49 +0800

    restat back 251201

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -2,9 +2,15 @@ import { defineClientConfig } from "vuepress/client";
 import Blog from "./layouts/Blog.vue";
 import { setupSnowFall } from "vuepress-theme-hope/presets/snowFall.js";
 import { setupRunningTimeFooter } from "vuepress-theme-hope/presets/footerRunningTime.js";
-
-
-
+// .vuepress/client.js
+// 基础样式
+import 'vidstack/player/styles/base.css';
+// 主题样式
+import 'vidstack/player/styles/default/theme.css';
+// 音频播放器布局样式
+import 'vidstack/player/styles/default/layouts/audio.css';
+// 视频播放器布局样式（如果用到视频）
+import 'vidstack/player/styles/default/layouts/video.css';
 export default defineClientConfig({
   // 注册自定义布局
   layouts: {
